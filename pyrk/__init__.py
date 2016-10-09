7cab2889018c8fcc3d76d3b6c5e596ef79a4ba91
walchko <kevin.walchko@outlook.com>
2016-10-09 14:04:24 -0600

    travis fix

--- pyrk/__init__.py
+++ pyrk/__init__.py
@@ -1,9 +1,9 @@
 # from . import pyrk
-from pkg_resources import get_distribution
+# from pkg_resources import get_distribution
 # from pyrk.pyrk import RK4
 from pyrk import RK4
 
-__version__ = get_distribution('pyrk').version
+# __version__ = get_distribution('pyrk').version
 __license__ = 'MIT'
 __copyright__ = 'copyright (c) 2016 Kevin J. Walchko'
 __author__ = 'Kevin J. Walchko'
