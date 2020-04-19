a4bd9bc888c8e5f59bd17012df5f9d7dce796d7a
walchko <walchko@users.noreply.github.com>
2020-04-19 11:35:32 -0400

    poetry :bento: :bee: :snake:

--- pyrk/__init__.py
+++ pyrk/__init__.py
@@ -1,8 +1,17 @@
-# from . import pyrk
-# from pkg_resources import get_distribution
-# from pyrk.pyrk import RK4
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2015 Kevin Walchko
+# see LICENSE for full details
+##############################################
 from .pyrk import RK4
-from .version import __version__
+
+
+try:
+    from importlib.metadata import version # type: ignore
+except ImportError:
+    from importlib_metadata import version # type: ignore
+
 __license__ = 'MIT'
 __copyright__ = 'copyright (c) 2016 Kevin J. Walchko'
 __author__ = 'Kevin J. Walchko'
+__version__ = version("pyrk")
