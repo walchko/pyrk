e69cb5d6c44fb171b3f21e7db426ffe48bbd291e
walchko <walchko@users.noreply.github.com>
2020-04-19 22:11:00 -0400

    up

--- pyrk/pyrk.py
+++ pyrk/pyrk.py
@@ -3,24 +3,34 @@
 # Copyright (c) 2016 Kevin Walchko
 # see LICENSE for full details
 ##############################################
+import attr
 
-class RK4(object):
+@attr.s(slots=True)
+class RK4:
     """
     Implements a Runge-Kutta 4 as explained here:
     https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods
+
+    f - function(t, y, u)
+    where:
+        t - time
+        y - state
+        u - control force
     """
 
-    def __init__(self, f):
-        """
-        Constructor which takes a function for integration.
+    # def __init__(self, f):
+    #     """
+    #     Constructor which takes a function for integration.
+    #
+    #     f - function(t, y, u)
+    #         where:
+    #             t - time
+    #             y - state
+    #             u - control force
+    #     """
+    #     self.func = f
 
-        f - function(t, y, u)
-            where:
-                t - time
-                y - state
-                u - control force
-        """
-        self.func = f
+    f = attr.ib()
 
     def solve(self, y, h, t_end):
         """
@@ -58,3 +68,7 @@ class RK4(object):
         k3 = h * self.func(t + .5*h, y + .5*h*k2, u)
         k4 = h * self.func(t + h, y + h*k3, u)
         return y + (k1 + 2*k2 + 2*k3 + k4) / 6.0
+
+    def __call__(self, y, u, t, h):
+        """Alternative to calling step()"""
+        return self.step(y, u, t, h)
